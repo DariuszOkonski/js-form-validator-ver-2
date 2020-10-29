0566572c006c5135ecc004c7dc93d23ab498fc4d
Dariusz <darek200180@gmail.com>
2020-10-29 23:01:16 +0000

    set all elements at domElement object

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const domElements = {
+    btnSubmit: document.getElementById('btn-submit'),
+    btnBack: document.getElementById('btn-back'),
+    cover: document.getElementById('cover'),
+    username: document.getElementById('username'),
+    email: document.getElementById('email'),
+    password: document.getElementById('password'),
+    rePassword: document.getElementById('rePassword')
+};
+
+console.log(domElements);
