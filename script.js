c78dc18b158cfd917f878f86d8c2cfe8e1d0de35
Dariusz <darek200180@gmail.com>
2020-10-29 23:49:13 +0000

    add check for emptines and if valid end

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const domElements = {
     email: document.getElementById('email'),
     password: document.getElementById('password'),
     rePassword: document.getElementById('rePassword'),
-    form: document.getElementById('form')
+    form: document.getElementById('form'),
+    formGroups: document.querySelectorAll('.form-group')
 };
 
 function showError(element, message) {
@@ -15,14 +16,70 @@ function showError(element, message) {
     
     const small = parent.querySelector('small');
     small.innerText = message;
+
+    domElements.allValid = false;
+}
+
+function showSuccess(element) {
+    const parent = element.parentNode;
+    parent.classList = 'form-group success';
+
+    domElements.allValid = true;
+}
+
+function checkIsEmpty() {
+    if(username.value === '') {
+        showError(username, 'Username can not be empty');
+    } else {
+        showSuccess(username);
+    }
+    
+    if(email.value === '') {
+        showError(email, 'Email can not be empty');
+    } else {
+        showSuccess(email);
+    }
+    
+    if(password.value === '') {
+        showError(password, 'Password can not be empty');
+    } else {
+        showSuccess(password);
+    }
+    
+    if(rePassword.value === '') {
+        showError(rePassword, 'Re-password can not be empty');
+    } else {
+        showSuccess(rePassword);
+    }
+}
+
+function checkIfAllValid() {
+    // console.log(domElements.formGroups);
+    let isValid = true;
+    domElements.formGroups.forEach(element => {
+        if(element.classList.contains('error')) {
+            isValid = false;
+            return;
+        }
+            
+    });
+    return isValid;
 }
 
 domElements.form.addEventListener('click', (e) => {
     e.preventDefault();
 
-    if(username.value === '') {
-        showError(username, 'element can not be empty');
-    } else {
+    checkIsEmpty();
 
+    // console.log(checkIfAllValid());
+
+    if(checkIfAllValid()) {
+        setTimeout(() => {
+            domElements.cover.classList = 'cover';            
+        }, 2000);
     }
+})
+
+domElements.btnBack.addEventListener('click', () => {
+    window.location.reload(true);
 })
