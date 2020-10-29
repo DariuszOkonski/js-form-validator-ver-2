b610e2aaffdfda39877baf645281cafa959ff9aa
Dariusz <darek200180@gmail.com>
2020-10-29 23:09:36 +0000

    add first if

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ const domElements = {
     username: document.getElementById('username'),
     email: document.getElementById('email'),
     password: document.getElementById('password'),
-    rePassword: document.getElementById('rePassword')
+    rePassword: document.getElementById('rePassword'),
+    form: document.getElementById('form')
 };
 
-console.log(domElements);
+function showError(element, message) {
+    const parent = element.parentNode;
+    parent.classList = 'form-group error';
+    
+    const small = parent.querySelector('small');
+    small.innerText = message;
+}
+
+domElements.form.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if(username.value === '') {
+        showError(username, 'element can not be empty');
+    } else {
+
+    }
+})
